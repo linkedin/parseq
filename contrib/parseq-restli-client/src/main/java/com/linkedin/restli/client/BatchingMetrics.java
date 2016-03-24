31a51649b9fb2c5c0087b81a424eaa0756365866
jodzga <jodzga@linkedin.com>
2016-03-24 13:49:02 -0700

    Added consumer for new metrics to BatchingMetrics.

--- contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/BatchingMetrics.java
+++ contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/BatchingMetrics.java
@@ -1,19 +1,33 @@
 package com.linkedin.restli.client;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.BiConsumer;
 
 import com.linkedin.parseq.batching.BatchSizeMetric;
 
 public class BatchingMetrics {
   private final ConcurrentMap<String, BatchSizeMetric> batchSizePerEndpoint = new ConcurrentHashMap<>();
 
+  private final ConcurrentLinkedQueue<BiConsumer<String, BatchSizeMetric>> _metricsConsumers =
+      new ConcurrentLinkedQueue<>();
+
   public void recordBatchSize(String endpoint, int batchSize) {
-    BatchSizeMetric metric = batchSizePerEndpoint.computeIfAbsent(endpoint, k -> new BatchSizeMetric());
+    final BatchSizeMetric metric = batchSizePerEndpoint.computeIfAbsent(endpoint, k -> {
+      final BatchSizeMetric newMetric = new BatchSizeMetric();
+      _metricsConsumers.forEach(consumer -> consumer.accept(k, newMetric));
+      return newMetric;
+    });
     metric.record(batchSize);
   }
 
   public ConcurrentMap<String, BatchSizeMetric> getBatchSizeMetrics() {
     return batchSizePerEndpoint;
   }
+
+  public void addNewEndpointMetricConsumer(BiConsumer<String, BatchSizeMetric> consumer) {
+    _metricsConsumers.add(consumer);
+  }
+
 }
