0d95af506f0ee030ded555384b059590beec1d1e
jodzga <jodzga@linkedin.com>
2016-07-13 16:22:14 -0700

    Implementation  of MultipleRequestConfigProvider.

--- contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/MultipleRequestConfigProvider.java
+++ contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/MultipleRequestConfigProvider.java
@@ -1,17 +1,36 @@
-package com.linkedin.restli.client.config;
+package com.linkedin.restli.client;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.linkedin.restli.client.Request;
+import com.linkedin.restli.client.config.RequestConfig;
+import com.linkedin.restli.client.config.RequestConfigProvider;
 
-class MultipleRequestConfigProvider implements RequestConfigProvider {
+class MultipleRequestConfigProvider<T extends Enum<T>> implements RequestConfigProvider {
 
-  public MultipleRequestConfigProvider() {
+  private final Map<T, ParSeqRestClientConfig> _configs;
+  private final ParSeqRestClientConfigChooser<T> _chooser;
+  private final InboundRequestContextFinder _inboundRequestContextFinder;
 
+  private final ConcurrentHashMap<T, RequestConfigProvider> _providers = new ConcurrentHashMap<>();
+
+  public MultipleRequestConfigProvider(Map<T, ParSeqRestClientConfig> configs,
+      ParSeqRestClientConfigChooser<T> chooser, InboundRequestContextFinder inboundRequestContextFinder) {
+    _configs = configs;
+    _chooser = chooser;
+    _inboundRequestContextFinder = inboundRequestContextFinder;
+  }
+
+  private RequestConfigProvider getProvider(T type) {
+    return RequestConfigProvider.build(_configs.get(type), _inboundRequestContextFinder);
   }
 
   @Override
   public RequestConfig apply(Request<?> request) {
-    // TODO Auto-generated method stub
-    return null;
+    T type = _chooser.apply(_inboundRequestContextFinder.find(), request);
+    RequestConfigProvider provider = _providers.computeIfAbsent(type, this::getProvider);
+    return provider.apply(request);
   }
 
 }
