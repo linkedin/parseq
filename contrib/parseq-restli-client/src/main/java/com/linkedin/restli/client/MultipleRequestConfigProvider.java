62be7273e5d53e41b8ce43e94323d8cb7e3cd5ba
jodzga <jodzga@linkedin.com>
2016-07-18 16:19:05 -0700

    Changed ParSeqRestClientConfigChooser interface to return String instead of enum.

--- contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/MultipleRequestConfigProvider.java
+++ contrib/parseq-restli-client/src/main/java/com/linkedin/restli/client/MultipleRequestConfigProvider.java
@@ -7,28 +7,28 @@ import com.linkedin.restli.client.Request;
 import com.linkedin.restli.client.config.RequestConfig;
 import com.linkedin.restli.client.config.RequestConfigProvider;
 
-class MultipleRequestConfigProvider<T extends Enum<T>> implements RequestConfigProvider {
+class MultipleRequestConfigProvider implements RequestConfigProvider {
 
-  private final Map<T, ParSeqRestClientConfig> _configs;
-  private final ParSeqRestClientConfigChooser<T> _chooser;
+  private final Map<String, ParSeqRestClientConfig> _configs;
+  private final ParSeqRestClientConfigChooser _chooser;
   private final InboundRequestContextFinder _inboundRequestContextFinder;
 
-  private final ConcurrentHashMap<T, RequestConfigProvider> _providers = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, RequestConfigProvider> _providers = new ConcurrentHashMap<>();
 
-  public MultipleRequestConfigProvider(Map<T, ParSeqRestClientConfig> configs,
-      ParSeqRestClientConfigChooser<T> chooser, InboundRequestContextFinder inboundRequestContextFinder) {
+  public MultipleRequestConfigProvider(Map<String, ParSeqRestClientConfig> configs,
+      ParSeqRestClientConfigChooser chooser, InboundRequestContextFinder inboundRequestContextFinder) {
     _configs = configs;
     _chooser = chooser;
     _inboundRequestContextFinder = inboundRequestContextFinder;
   }
 
-  private RequestConfigProvider getProvider(T type) {
+  private RequestConfigProvider getProvider(String type) {
     return RequestConfigProvider.build(_configs.get(type), _inboundRequestContextFinder);
   }
 
   @Override
   public RequestConfig apply(Request<?> request) {
-    T type = _chooser.apply(_inboundRequestContextFinder.find(), request);
+    String type = _chooser.apply(_inboundRequestContextFinder.find(), request);
     RequestConfigProvider provider = _providers.computeIfAbsent(type, this::getProvider);
     return provider.apply(request);
   }
