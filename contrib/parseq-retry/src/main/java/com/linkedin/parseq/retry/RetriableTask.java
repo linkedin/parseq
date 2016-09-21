02ac59fbfaf9b3b2ecd406a5ad66a3f5065022ba
jodzga <jodzga@linkedin.com>
2016-09-21 13:35:38 -0500

    Changed j.u.Function to c.l.p.f.Function1 that throws an Exception. Added unit test for a retriable task in which function throws checked exception.

--- contrib/parseq-retry/src/main/java/com/linkedin/parseq/retry/RetriableTask.java
+++ contrib/parseq-retry/src/main/java/com/linkedin/parseq/retry/RetriableTask.java
@@ -1,18 +1,19 @@
 package com.linkedin.parseq.retry;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.linkedin.parseq.Context;
 import com.linkedin.parseq.Priority;
 import com.linkedin.parseq.Task;
+import com.linkedin.parseq.function.Function1;
 import com.linkedin.parseq.internal.ArgumentUtil;
 import com.linkedin.parseq.promise.Promise;
 import com.linkedin.parseq.promise.Promises;
 import com.linkedin.parseq.promise.SettablePromise;
-
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -28,7 +29,7 @@ public final class RetriableTask<T> {
   private final String _name;
 
   /** A task generator function. It will receive a zero-based attempt number as a parameter. */
-  private final Function<Integer, Task<T>> _taskFunction;
+  private final Function1<Integer, Task<T>> _taskFunction;
 
   /** Retry policy which will control this task's behavior. */
   private final RetryPolicy _policy;
@@ -43,7 +44,7 @@ public final class RetriableTask<T> {
    * @param policy Retry policy that will control this task's behavior.
    * @param taskFunction A task generator function. It will receive a zero-based attempt number as a parameter.
    */
-  private RetriableTask(String name, RetryPolicy policy, Function<Integer, Task<T>> taskFunction)
+  private RetriableTask(String name, RetryPolicy policy, Function1<Integer, Task<T>> taskFunction)
   {
     ArgumentUtil.requireNotNull(name, "name");
     ArgumentUtil.requireNotNull(policy, "policy");
@@ -61,8 +62,8 @@ public final class RetriableTask<T> {
    * @param taskSupplier A task generator function.
    * @param <U> Type of a task result.
    */
-  public static <U> Task<U> withRetryPolicy(RetryPolicy policy, Supplier<Task<U>> taskSupplier) {
-    return withRetryPolicy("operation", policy, attempt -> taskSupplier.get());
+  public static <U> Task<U> withRetryPolicy(RetryPolicy policy, Callable<Task<U>> taskSupplier) {
+    return withRetryPolicy("operation", policy, attempt -> taskSupplier.call());
   }
 
   /**
@@ -72,7 +73,7 @@ public final class RetriableTask<T> {
    * @param taskFunction A task generator function. It will receive a zero-based attempt number as a parameter.
    * @param <U> Type of a task result.
    */
-  public static <U> Task<U> withRetryPolicy(RetryPolicy policy, Function<Integer, Task<U>> taskFunction) {
+  public static <U> Task<U> withRetryPolicy(RetryPolicy policy, Function1<Integer, Task<U>> taskFunction) {
     return withRetryPolicy("operation", policy, taskFunction);
   }
 
@@ -84,8 +85,8 @@ public final class RetriableTask<T> {
    * @param taskSupplier A task generator function.
    * @param <U> Type of a task result.
    */
-  public static <U> Task<U> withRetryPolicy(String name, RetryPolicy policy, Supplier<Task<U>> taskSupplier) {
-    return withRetryPolicy(name, policy, attempt -> taskSupplier.get());
+  public static <U> Task<U> withRetryPolicy(String name, RetryPolicy policy, Callable<Task<U>> taskSupplier) {
+    return withRetryPolicy(name, policy, attempt -> taskSupplier.call());
   }
 
   /**
@@ -96,17 +97,17 @@ public final class RetriableTask<T> {
    * @param taskFunction A task generator function. It will receive a zero-based attempt number as a parameter.
    * @param <U> Type of a task result.
    */
-  public static <U> Task<U> withRetryPolicy(String name, RetryPolicy policy, Function<Integer, Task<U>> taskFunction) {
+  public static <U> Task<U> withRetryPolicy(String name, RetryPolicy policy, Function1<Integer, Task<U>> taskFunction) {
     RetriableTask<U> retriableTask = new RetriableTask<>(name, policy, taskFunction);
     return Task.async(name + " retriableTask", retriableTask::run);
   }
 
   /** Create a wrapped task with associated recovery task that will retry if necessary. */
   private Task<T> wrap(int attempt) {
-    Task<T> task = _taskFunction.apply(attempt);
-
     return Task.async(_policy.getName() + ", attempt " + attempt, context -> {
       final SettablePromise<T> result = Promises.settable();
+
+      Task<T> task = _taskFunction.apply(attempt);
 
       final Task<T> recovery = Task.async(_name + " recovery", recoveryContext -> {
         final SettablePromise<T> recoveryResult = Promises.settable();
