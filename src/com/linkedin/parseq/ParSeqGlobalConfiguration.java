49609b8da2a1ffdb5dc5637e3cec4c98ebf8e6b2
Oleg Anashkin <oanashkin@linkedin.com>
2017-03-01 12:38:59 -0800

    Code review feedback

--- src/com/linkedin/parseq/ParSeqGlobalConfiguration.java
+++ src/com/linkedin/parseq/ParSeqGlobalConfiguration.java
@@ -1,19 +1,29 @@
-package com.linkedin.parseq;
+/*
+ * Copyright 2012 LinkedIn, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-import java.util.concurrent.atomic.AtomicBoolean;
+package com.linkedin.parseq;
 
 
 /**
  * Global parseq configuration, applies to all Engine and Task instances
+ *
+ * @author Oleg Anashkin (oanashkin@linkedin.com)
  */
-public class ParSeqGlobalConfiguration {
-  private static final ParSeqGlobalConfiguration _instance = new ParSeqGlobalConfiguration();
-
-  private AtomicBoolean _crossThreadStackTracesEnabled = new AtomicBoolean(false);
-
-  public static ParSeqGlobalConfiguration getInstance() {
-    return _instance;
-  }
+public final class ParSeqGlobalConfiguration {
+  private static volatile boolean _crossThreadStackTracesEnabled = false;
 
   private ParSeqGlobalConfiguration() {
   }
@@ -21,30 +31,31 @@ public class ParSeqGlobalConfiguration {
   /**
    * Returns current state of cross-thread (cross-task) stack tracing.
    *
-   * Normally tasks are executed in different thread from the one creating it and at a different time. This makes it
-   * hard to debug because if task throws an exception, its call stack ends in the execution engine that actually
+   * Normally tasks are executed in a different thread from the one creating it and at a different time. This makes it
+   * hard to debug because if a task throws an exception, its call stack ends in the execution engine that actually
    * starts a thread that serves the task. This feature collects stack trace in advance, when task is created, so that
-   * if the task throws the exception then the parent stack trace is appended to it. This has a small performance
-   * impact even if the task doesn't throw any exception because stack trace is collected in task constructor.
+   * if a task throws an exception then the parent stack trace is appended to it. This has a small performance
+   * impact even if the task doesn't throw any exceptions because stack trace is collected in task constructor.
    *
    * @return true if cross-thread stack tracing is enabled, false otherwise
    */
-  public boolean isCrossThreadStackTracesEnabled() {
-    return _crossThreadStackTracesEnabled.get();
+  public static boolean isCrossThreadStackTracesEnabled() {
+    return _crossThreadStackTracesEnabled;
   }
 
   /**
    * Modifies the current state of cross-thread (cross-task) stack tracing.
+   * This is a dynamic runtime configuration that has immediate effect on all tasks in the current process.
    *
-   * Normally tasks are executed in different thread from the one creating it and at a different time. This makes it
-   * hard to debug because if task throws an exception, its call stack ends in the execution engine that actually
+   * Normally tasks are executed in a different thread from the one creating it and at a different time. This makes it
+   * hard to debug because if a task throws an exception, its call stack ends in the execution engine that actually
    * starts a thread that serves the task. This feature collects stack trace in advance, when task is created, so that
-   * if the task throws the exception then the parent stack trace is appended to it. This has a small performance
-   * impact even if the task doesn't throw any exception because stack trace is collected in task constructor.
+   * if a task throws an exception then the parent stack trace is appended to it. This has a small performance
+   * impact even if the task doesn't throw any exceptions because stack trace is collected in task constructor.
    *
    * @param enabled true if cross-thread stack tracing is enabled, false otherwise
    */
-  public void setCrossThreadStackTracesEnabled(boolean enabled) {
-    _crossThreadStackTracesEnabled.set(enabled);
+  public static void setCrossThreadStackTracesEnabled(boolean enabled) {
+    _crossThreadStackTracesEnabled = enabled;
   }
 }
