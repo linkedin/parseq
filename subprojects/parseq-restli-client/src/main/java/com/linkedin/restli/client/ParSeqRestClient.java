03aaff9094754ed380baf8150c7733cd133d7fcc
Francesco Capponi <fcapponi@linkedin.com>
2019-04-23 20:15:28 -0700

    In ParSeqRestClient align createTaskWithD2Timeout and createTaskWithTimeout to always run the timeout task with the same approach

--- subprojects/parseq-restli-client/src/main/java/com/linkedin/restli/client/ParSeqRestClient.java
+++ subprojects/parseq-restli-client/src/main/java/com/linkedin/restli/client/ParSeqRestClient.java
@@ -213,7 +213,7 @@ public class ParSeqRestClient extends BatchingStrategy<RequestGroup, RestRequest
 
   // check whether we need to apply timeout to a rest.li request task.
   private boolean needApplyTaskTimeout(RequestContext requestContext, ConfigValue<Long> timeout) {
-    // **return false** if no timeout configured or per-request timeout already specified in request context
+    // return false if no timeout configured or per-request timeout already specified in request context
     return timeout.getValue() != null && timeout.getValue() > 0 && !hasRequestContextTimeout(requestContext);
   }
 
