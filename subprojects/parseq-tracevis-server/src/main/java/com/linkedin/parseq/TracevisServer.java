54ec37a79f81797a319a90d9c33bf7272fba54db
Jiaqi Guan <jguan@linkedin.com>
2019-04-27 18:08:54 -0700

    Support HTTPS in parseq-tracevis-server

--- subprojects/parseq-tracevis-server/src/main/java/com/linkedin/parseq/TracevisServer.java
+++ subprojects/parseq-tracevis-server/src/main/java/com/linkedin/parseq/TracevisServer.java
@@ -6,14 +6,13 @@ import java.nio.file.Path;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +32,8 @@ public class TracevisServer {
   private final String _dotLocation;
   final GraphvizEngine _graphvizEngine;
 
+  protected Server _server;
+
   public TracevisServer(final String dotLocation, final int port, final Path baseLocation, final Path heapsterLocation,
       final int cacheSize, final long timeoutMs) {
     _dotLocation = dotLocation;
@@ -46,6 +47,7 @@ public class TracevisServer {
         Runtime.getRuntime().availableProcessors(), Constants.DEFAULT_REAPER_DELAY_MS,
         Constants.DEFAULT_PROCESS_QUEUE_SIZE);
   }
+
 
   public void start()
       throws Exception {
@@ -64,8 +66,9 @@ public class TracevisServer {
 
     _graphvizEngine.start();
 
-    Server server = new Server(_port);
-    server.setAttribute("org.eclipse.jetty.server.Request.maxFormContentSize", -1);
+    _server = new Server();
+    _server.setAttribute("org.eclipse.jetty.server.Request.maxFormContentSize", -1);
+    _server.setConnectors(getConnectors(_server));
 
     TracePostHandler tracePostHandler = new TracePostHandler(_staticContentLocation.toString());
 
@@ -85,20 +88,28 @@ public class TracevisServer {
         new JhatHandler(engine),
         tracePostHandler,
         traceHandler,
+        new HealthCheckHandler(),
         heapsterHandler,
         new DefaultHandler()
         });
-    server.setHandler(handlers);
+    _server.setHandler(handlers);
 
     try {
-      server.start();
-      server.join();
+      _server.start();
+      _server.join();
     } finally {
-      server.stop();
+      _server.stop();
       _graphvizEngine.stop();
       engine.shutdown();
       scheduler.shutdownNow();
       HttpClient.close();
     }
+  }
+
+  protected Connector[] getConnectors(Server server)
+  {
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(_port);
+    return new Connector[] { connector };
   }
 }
--- subprojects/parseq-tracevis-server/src/main/java/com/linkedin/parseq/TracevisServerJarMain.java
+++ subprojects/parseq-tracevis-server/src/main/java/com/linkedin/parseq/TracevisServerJarMain.java
@@ -1,6 +1,9 @@
 package com.linkedin.parseq;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLDecoder;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
@@ -8,8 +11,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Enumeration;
+import java.util.Properties;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.regex.Pattern;
+
 
 public class TracevisServerJarMain {
 
@@ -18,11 +24,12 @@ public class TracevisServerJarMain {
     if (args.length < 1 || args.length > 2) {
       System.out.println("Incorrect arguments, expecting: DOT_LOCATION <PORT>\n"
           + "  DOT_LOCATION - location of graphviz dot executable\n"
-          + "  <PORT>       - optional port number, default is " + Constants.DEFAULT_PORT);
+          + "  <PORT>       - optional port number, default is " + Constants.DEFAULT_PORT +
+          "OR <CONFIG_FILE> - optional SSL configuration file path for https");
       System.exit(1);
     }
+
     final String dotLocation = args[0];
-    final int port = (args.length == 2) ? Integer.parseInt(args[1]) : Constants.DEFAULT_PORT;
 
     String path = TracevisServerJarMain.class.getProtectionDomain().getCodeSource().getLocation().getPath();
     String onwJarFile = URLDecoder.decode(path, "UTF-8");
@@ -43,8 +50,34 @@ public class TracevisServerJarMain {
         }
       }
 
-      new TracevisServer(dotLocation, port, base, base, Constants.DEFAULT_CACHE_SIZE, Constants.DEFAULT_TIMEOUT_MS)
-        .start();
+      Pattern pattern = Pattern.compile("[0-9]{4}");
+      if (args.length == 1 || pattern.matcher(args[1]).matches()) { // support http only
+        int httpPort = args.length == 2 ? Integer.parseInt(args[1]) : Constants.DEFAULT_PORT;
+        new TracevisServer(dotLocation, httpPort, base, base, Constants.DEFAULT_CACHE_SIZE, Constants.DEFAULT_TIMEOUT_MS)
+            .start();
+      } else { // support both http and https
+
+        String fileName = args[1];
+        File configFile = new File(fileName);
+        if (configFile.exists()) {
+          Properties prop = new Properties();
+          InputStream in = new FileInputStream(fileName);
+          prop.load(in);
+
+          // get properties from specified config file
+          int httpPort = Integer.parseInt(prop.getProperty("httpPort", String.valueOf(Constants.DEFAULT_PORT)));
+          int sslPort = Integer.parseInt(prop.getProperty("sslPort", "8081"));
+          String keyStorePath = prop.getProperty("keyStorePath", "");
+          String keyStorePassword = prop.getProperty("keyStorePassword", "");
+          String trustStorePassword = prop.getProperty("trustStorePassword", "");
+          String trustStorePath = prop.getProperty("trustStorePath", "");
+
+          new TracevisHttpsServer(dotLocation, httpPort, base, base, Constants.DEFAULT_CACHE_SIZE, Constants.DEFAULT_TIMEOUT_MS, sslPort,
+              keyStorePath, keyStorePassword, trustStorePath, trustStorePassword).start();
+        } else {
+          throw new RuntimeException("Failed to find config profiles!");
+        }
+      }
 
     } finally {
       //delete base directory recursively
