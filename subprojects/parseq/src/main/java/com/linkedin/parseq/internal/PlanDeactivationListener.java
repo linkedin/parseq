0ee894aacd069548a7e9250264a3d84d625e675f
Hitesh Sharma <hitesh@slideshare.com>
2019-10-21 18:35:09 -0700

    put back comment to see setPlanDeactivationListener

--- subprojects/parseq/src/main/java/com/linkedin/parseq/internal/PlanDeactivationListener.java
+++ subprojects/parseq/src/main/java/com/linkedin/parseq/internal/PlanDeactivationListener.java
@@ -5,6 +5,7 @@ import com.linkedin.parseq.EngineBuilder;
 /**
  * This listener interface allows receiving notifications about plan deactivation.
  * It can be used for monitoring and to optimize task implementation e.g. to implement batching.
+ * @see EngineBuilder#setPlanDeactivationListener(PlanDeactivationListener) for details
  *
  * @author Jaroslaw Odzga (jodzga@linkedin.com)
  *
