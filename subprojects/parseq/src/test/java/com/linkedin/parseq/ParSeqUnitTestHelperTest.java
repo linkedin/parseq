e2e270d8798919662070c6ede09e2da3e045d732
Hitesh Sharma <hsharma@linkedin.com>
2020-07-30 17:54:22 -0700

    Added new runAndWaitForPlanToCompleteException to wait for plan to complete even when exception is expected

--- subprojects/parseq/src/test/java/com/linkedin/parseq/ParSeqUnitTestHelperTest.java
+++ subprojects/parseq/src/test/java/com/linkedin/parseq/ParSeqUnitTestHelperTest.java
@@ -1,8 +1,11 @@
 package com.linkedin.parseq;
 
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.assertTrue;
 
 public class ParSeqUnitTestHelperTest {
 
@@ -14,4 +17,23 @@ public class ParSeqUnitTestHelperTest {
         assertThrows(AssertionError.class, () -> ParSeqUnitTestHelper.assertEquals("x", null));
         assertThrows(AssertionError.class, () -> ParSeqUnitTestHelper.assertEquals("x", "y"));
     }
+
+  @Test(description = "Proves that runAndWaitForPlanToCompleteException verifies the expected exception is thrown "
+    + "while also waiting for entire plan to complete")
+  public void testRunAndWaitForPlanToCompleteException() throws Exception {
+    ParSeqUnitTestHelper parSeqUnitTestHelper = new ParSeqUnitTestHelper();
+    parSeqUnitTestHelper.setUp();
+
+    Task<String> delayedSideEffect = parSeqUnitTestHelper.delayedValue("delayed", 1, TimeUnit.SECONDS);
+    parSeqUnitTestHelper.runAndWaitForPlanToCompleteException(
+      Task.par(
+        Task.value("a").withSideEffect(a -> delayedSideEffect),
+        parSeqUnitTestHelper.delayedFailure(new Exception(), 100, TimeUnit.MILLISECONDS)
+      ),
+      Exception.class
+    );
+    // This assertion proves that entire plan is completing
+    assertTrue(delayedSideEffect.isDone());
+    parSeqUnitTestHelper.tearDown();
+  }
 }
